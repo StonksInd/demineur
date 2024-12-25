76394422e6b773022781eceff825ae365e7e576a
Nath <nathlej41@gmail.com>
2024-12-25 18:43:33 +0100

    3

--- code/index.js
+++ code/index.js
@@ -1,11 +1,11 @@
 !1 // ! CREER UNE FONCTION QUI VA AFFFICHER UN TABLEAU ET QUI PREND EN ENTREE Lxl CASES
-console.log("ici");
+
 function crc(row, collumn) { //create_grid_demineur
-    console.log("ici");
+
 
     for (let i = 0; i < row; i++) {
 
-        console.log("ici");
+
         let new_row = document.createElement("ul");
         new_row.setAttribute("id", "row" + i)
         let demineur_grid_row = document.getElementById('demineur_grid');
@@ -16,19 +16,19 @@ function crc(row, collumn) { //create_grid_demineur
 
             let new_collumn = document.createElement("button");
             new_collumn.setAttribute("id", "row" + i + "collumn" + j)
-            new_collumn.textContent = "0";
+            new_collumn.textContent = "00";
             let demineur_grid_collumn = document.getElementById("row" + i);
             demineur_grid_collumn.appendChild(new_collumn);
 
         }
 
-    } console.log("ici");
+    }
 
 
 
 
 }
-
+crc(10, 10);
 
 //! 2 CREER UNE FONCTION QUI VA AVOIR EN ENTREE 2 NOMBRES DE COORDONNEE ALLEATOIRE EN FONCTION DE L ET l
 //!   ET QUI VA PLACER X NOMBRE DE BOMBE EN STOCKANT LES CO DANS UN TAB POUR PAS QU4ILL Y AI DE REDONDANCES 
@@ -49,25 +49,45 @@ function cb(row, collumn, nbr_of_bomb) { //create_bomb
             coord_bomb_placed.push("row" + row_bomb + "collumn" + collumn_bomb);
             let bomb = document.getElementById("row" + row_bomb + "collumn" + collumn_bomb);
             bomb.textContent = "10";
-            console.log(row_bomb, collumn_bomb, i)
-            i++
+            //console.log(row_bomb, collumn_bomb, i);
+            i++;
 
 
         }
 
     }
 }
+cb(10, 10, 12);
 
 
 
 //! 3 CREER UNE FONCTION QUI VA PARCOURRIR LE TABLEAU POUR CHAQUE CASES ET VA REGARDER LES BOMBES PRESENTES
 //!   DANS LES CASES ADJACENTE ET VA METTRE UN NOMBRE
 
-function create_nbr_above_bomb(row, collumn) {
+
+function cnab(row, collumn) {//create_nbr_above_bomb
+
+    tab_3x3_coord_case = [[1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1]];
     for (let i = 0; i < row; i++) {
-        for (let j = 0; j < collumn; j++) {
+        for (let j = 0; j < collumn; j++) {//pour chaque cases
+            if (document.getElementById("row" + i + "collumn" + j).textContent != "10") {
+                let bomb_count = 0;
+                tab_3x3_coord_case.forEach(coord => {
+                    if ((i + coord[0]) < row && (i + coord[0]) >= 0 && (j + coord[1]) < collumn && (j + coord[1]) >= 0) {//on s'assure que notre case sur laquelle on va cherche existe / n'est pas dans une bordure
+
+                        if (document.getElementById("row" + (i + coord[0]) + "collumn" + (j + coord[1])).textContent == "10") {
+                            bomb_count++;
 
 
+                        }
+
+                    }
+
+                });
+                document.getElementById("row" + i + "collumn" + j).textContent = "0" + bomb_count;
+                console.log(i, j, bomb_count);
+
+            } else (console.log("il y a une bombe en ", i, j))
         }
 
     }
